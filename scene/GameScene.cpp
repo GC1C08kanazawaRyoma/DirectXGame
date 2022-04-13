17f47dd0aa370a62aa14c0ab3f2acd31465e6c4b
ryomaaaaa <k021g1231@g.neec.ac.jp>
2022-04-13 18:15:53 +0900

    02_01_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,20 +6,71 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { delete model_; }
 
-void GameScene::Initialize() {
-
+void GameScene::Initialize()
+{
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォーム初期化
+	worldTransform_.Initialize();
+
+	// ビュープロジェクション初期化
+	viewProjection_.Initialize();
+
+	// x、y、z 方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+
+	//x、y、z 軸周りの回転角を設定
+	worldTransform_.rotation_ = {XMConvertToRadians(45.0f), XMConvertToRadians(45.0f), 0.0f};
+
+	// x、y、z 軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	// ワールドトランスフォーム初期化
+	worldTransform_.Initialize();
 }
 
-void GameScene::Update() {}
+void GameScene::Update()
+{
+	debugText_->SetPos(50, 50);
+	debugText_->Printf
+	(
+		"translation:(%f,%f,%f)",
+		worldTransform_.translation_.x,
+		worldTransform_.translation_.y,
+		worldTransform_.translation_.z
+	);
 
-void GameScene::Draw() {
+	debugText_->SetPos(50, 70);
+	debugText_->Printf
+	(
+	  "rotetion:(%f,%f,%f)",
+		worldTransform_.rotation_.x,
+		worldTransform_.rotation_.y,
+		worldTransform_.rotation_.z
+	);
 
+	debugText_->SetPos(50, 90);
+	debugText_->Printf
+	(
+	  "scale:(%f,%f,%f)",
+		worldTransform_.scale_.x,
+		worldTransform_.scale_.y,
+		worldTransform_.scale_.z
+	);
+}
+
+void GameScene::Draw()
+{
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
@@ -44,6 +95,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	
+	/// // 3Dモデルの描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
